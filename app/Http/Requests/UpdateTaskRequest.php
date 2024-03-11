f548e7259f38c72869f2cef9676d090e8b96e271
perfectprofesionist <perfectprofesionist@gmail.com>
2024-03-11 13:06:42 +0530

    sanctum API auth

---
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\StoreTaskRequest;
+
+class UpdateTaskRequest extends StoreTaskRequest
+{
+    
+}
---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\V1\CompleteTaskController;
+
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function(){
+    Route::apiResource('/tasks', TaskController::class);
+    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
+});
+
+Route::prefix('auth')->group(function(){
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('/register', RegisterController::class);
+});
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
