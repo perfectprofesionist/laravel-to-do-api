bfd0ff9951d8258352727814f921b83763f6bcc4
perfectprofesionist <perfectprofesionist@gmail.com>
2024-03-19 18:04:32 +0530

    task summary api integration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Carbon\CarbonPeriod;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,16 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $start = now()->startOfMonth()->subMonthsNoOverflow();
+        $end = now();
+        $period = CarbonPeriod::create($start, '1 day', $end);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        User::factory(5)->has(
-            Task::factory(10)
-        )->create();
-        
+        User::factory(5)->create()->each(function ($user) use($period) {
+            foreach ($period as $date) {
+                $date->hour(rand(0, 23))->minute(rand(0, 6) * 10);
+                
+                Task::factory()->create([
+                    'user_id' => $user->id,
+                    'created_at' => $date,
+                    'updated_at' => $date
+                ]);
+            }
+        });
     }
 }
